aca03c5679b42e68e4010c2fd667ce67e53afab7
Arlene <kara_arlene@yahoo.com>
2017-07-14 09:38:37 -0600

    Add files via upload
    Update of files, and adding from https://codepen.io/ArleneAndrews/pen/BpbyJo

---
+++ portfolio.js
@@ -0,0 +1,54 @@
+html {
+    background: silver;
+}
+.main {
+    background: silver;
+}
+
+.img {
+    width: 300 px;
+    border - radius: 30 % ;
+}
+.h2 {
+    text - align: center;
+}#
+face {
+    border - radius: 20 % ;
+    width: 100 px;
+
+}
+.icon {
+    width: 70 px;
+    height: 70 px;
+}
+.extraText {
+    float: right;
+    width: 600 px;
+    padding: 0,
+    20,
+    0,
+    20;
+    font size: 20 px;
+    text - align: left;
+}
+.navbar {
+    height: 40 px;
+    width: auto;
+    text - align: right;
+    position: fixed;
+}
+.navbar a {
+    width: 55 px;
+    height: 20 px;
+    background - color: silver;
+    display: inline - block;
+}
+
+ul {
+    position: fixed;
+    bottom: 0;
+    width: 100 % ;
+    list - style - type: none;
+    padding: 20 px;
+
+}
